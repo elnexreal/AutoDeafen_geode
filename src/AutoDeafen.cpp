5220e71fe3229d78bb387fdd0a44c01e5531ddb4
elnexreal <40328573+elnexreal@users.noreply.github.com>
2024-02-28 18:37:34 -0300

    ok so, discord doesn't detect when pressing CTRL + SHIFT + D when u're not in the app

--- src/AutoDeafen.cpp
+++ src/AutoDeafen.cpp
@@ -10,10 +10,10 @@ void AutoDeafen::toggleDeafen() {
 	// Simulate CTRL + LEFT SHIFT + F12 key press
 	keybd_event(VK_CONTROL, 0, 0, 0); // Press down CTRL key
 	keybd_event(VK_LSHIFT, 0, 0, 0);  // Press down LEFT SHIFT key
-	keybd_event(0x44, 0, 0, 0);		  // Press down D key
+	keybd_event(VK_F12, 0, 0, 0);	  // Press down F12 key
 
 	// Simulate CTRL + LEFT SHIFT + F12 key release
-	keybd_event(0x44, 0, KEYEVENTF_KEYUP, 0);		// Release D key
+	keybd_event(VK_F12, 0, KEYEVENTF_KEYUP, 0);		// Release F12 key
 	keybd_event(VK_LSHIFT, 0, KEYEVENTF_KEYUP, 0);	// Release LEFT SHIFT key
 	keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0); // Release CTRL key
 }
