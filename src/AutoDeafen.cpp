5a4b694acda3a8b14215025f99e8c56325998580
elnexreal <40328573+elnexreal@users.noreply.github.com>
2024-02-28 21:51:10 -0300

    setted a default keybind for deafening (this will change)

--- src/AutoDeafen.cpp
+++ src/AutoDeafen.cpp
@@ -7,13 +7,19 @@ bool AutoDeafen::enabled = false;
 void AutoDeafen::toggleDeafen() {
 	enabled = !enabled;
 
-	// Simulate CTRL + LEFT SHIFT + F12 key press
-	keybd_event(VK_CONTROL, 0, 0, 0); // Press down CTRL key
-	keybd_event(VK_LSHIFT, 0, 0, 0);  // Press down LEFT SHIFT key
-	keybd_event(VK_F12, 0, 0, 0);	  // Press down F12 key
+	INPUT ip;
+	ip.type = INPUT_KEYBOARD;
+	ip.ki.wScan = 0;
+	ip.ki.time = 0;
+	ip.ki.dwExtraInfo = 0;
 
-	// Simulate CTRL + LEFT SHIFT + F12 key release
-	keybd_event(VK_F12, 0, KEYEVENTF_KEYUP, 0);		// Release F12 key
-	keybd_event(VK_LSHIFT, 0, KEYEVENTF_KEYUP, 0);	// Release LEFT SHIFT key
-	keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0); // Release CTRL key
+	// press the key
+	ip.ki.wVk = VK_PRIOR;
+	ip.ki.dwFlags = 0;
+	SendInput(1, &ip, sizeof(INPUT));
+
+	// release the key
+	ip.ki.wVk = VK_PRIOR;
+	ip.ki.dwFlags = KEYEVENTF_KEYUP;
+	SendInput(1, &ip, sizeof(INPUT));
 }
